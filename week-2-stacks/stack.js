4b5e6ae344a8c2d8e49d795e750e79ed18b7ba6d
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-10-12 10:37:32 -0500

    added push method

--- week-2-stacks/stack.js
+++ week-2-stacks/stack.js
@@ -1,4 +1,13 @@
-class Stack {}
+class Stack {
+  constructor() {
+    this.storage = [];
+  }
+
+  push(book) {
+    this.storage.push(book);
+  }
+}
 
 const books = new Stack();
+books.push("Dark Matter");
 console.log(books);
