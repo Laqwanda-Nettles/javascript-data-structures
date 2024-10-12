44fad6144381ad37e097e3c101e2a941e900348b
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-10-12 10:42:56 -0500

    added pop method

--- week-2-stacks/stack.js
+++ week-2-stacks/stack.js
@@ -6,8 +6,18 @@ class Stack {
   push(book) {
     this.storage.push(book);
   }
+
+  pop() {
+    return this.storage.pop();
+  }
 }
 
 const books = new Stack();
 books.push("Dark Matter");
+books.push("Naked Lunch");
+books.push("The Ultimate Hithiker's Guide to the Galaxy");
+books.push("Burroughs");
 console.log(books);
+
+let b = books.pop();
+console.log(b);
