759b4bf0973be372d700f701e346211e9b70197c
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-10-26 10:23:18 -0500

    added queue and stack

--- week-3-palindrome/palindrome.js
+++ week-3-palindrome/palindrome.js
@@ -1,37 +1,73 @@
 // TODO: Create Stack and Queue classes.
 
 class Queue {
+  constructor() {
+    this.storage = [];
+  }
 
+  enqueue(item) {
+    this.storage.push(item);
+  }
+
+  dequeue() {
+    return this.storage.shift();
+  }
+
+  peek() {
+    return this.storage[0];
+  }
+
+  isEmpty() {
+    return this.storage.length === 0 ? true : false;
+  }
 }
 
 class Stack {
+  constructor() {
+    this.storage = [];
+  }
 
+  push(item) {
+    this.storage.push(item);
+  }
+
+  pop() {
+    return this.storage.pop();
+  }
+
+  peek() {
+    return this.storage[this.storage.length - 1];
+  }
+
+  isEmpty() {
+    return this.storage.length === 0 ? true : false;
+  }
 }
 
 function isPalindrome(str) {
   // TODO: instantiate the stack and queue
-  let stack ;
-  let queue ;
+  let stack = new Stack();
+  let queue = new Queue();
 
   // TODO: Step 1: Sanitize the input
-  let sanitizedStr; 
+  let sanitizedStr;
 
   // TODO: Step 2: Push characters onto stack and enqueue them into queue
-  for (let i=0; i< sanitizedStr.length; i++) {
-    let char = sanitizedStr[i]; 
+  for (let i = 0; i < sanitizedStr.length; i++) {
+    let char = sanitizedStr[i];
 
-
-
+    stack.push(char);
+    queue.enqueue(char);
   }
 
   // TODO: Step 3: Compare characters by popping from stack and dequeuing from queue
   while (!stack.isEmpty()) {
-    let a;
-    let b;
+    let a = stack.pop();
+    let b = queue.dequeue();
 
-      if (a !== b) {
-          return "The string is not a palindrome.";
-      }
+    if (a !== b) {
+      return "The string is not a palindrome.";
+    }
   }
 
   return "The string is a palindrome.";
